b8b93481e8ca3b9b3002919be321761366465dcd
jyotishman-web <jyotishmanpathak25@gmail.com>
2026-03-04 10:26:26 +0530

    Add files via upload

---
+++ public/games/Othello/manifest.ts
@@ -0,0 +1,75 @@
+import { ManifestOptions } from "vite-plugin-pwa";
+
+export const manifest: Partial<ManifestOptions> = {
+  name: "Othello",
+  short_name: "Othello",
+  description: "Play Othello against an AI with this beautiful modern web app.",
+  id: "othello-web-app",
+  background_color: "#121212",
+  theme_color: "#121212",
+  display: "standalone",
+  categories: ["games"],
+  iarc_rating_id: "b5c3da86-e1a0-4f97-86b6-a3f6246c90ff",
+  related_applications: [
+    {
+      platform: "play",
+      url: "https://play.google.com/store/apps/details?id=app.web.othello_rust.twa",
+      id: "app.web.othello_rust.twa",
+    },
+    {
+      platform: "windows",
+      url: "https://www.microsoft.com/store/productId/9N6P75JT9G40",
+    },
+  ],
+  icons: getIcons(),
+  screenshots: getScreenshots(),
+};
+
+function getIcons() {
+  const purposes = ["any", "maskable"];
+  const sizes = ["192", "512"];
+  const types = ["png", "webp"];
+
+  const icons: { src: string; sizes: string; type: string; purpose: string }[] = [];
+  for (const purpose of purposes) {
+    for (const type of types) {
+      for (const size of sizes) {
+        icons.push({
+          src: `icons/${purpose}-${size}.${type}`,
+          sizes: `${size}x${size}`,
+          type: `image/${type}`,
+          purpose: purpose,
+        });
+      }
+    }
+    icons.push({
+      src: `icons/${purpose}.svg`,
+      sizes: "any",
+      type: "image/svg+xml",
+      purpose,
+    });
+  }
+
+  return icons;
+}
+
+function getScreenshots() {
+  return [
+    ...[0, 1, 2, 3, 4, 5].map((id) => {
+      return {
+        src: `screenshots/android/${id}.webp`,
+        sizes: "1080x1920",
+        type: "image/webp",
+        platform: "android",
+      };
+    }),
+    ...[0, 1, 2, 3, 4, 5].map((id) => {
+      return {
+        src: `screenshots/windows/${id}.webp`,
+        sizes: "2560x1440",
+        type: "image/webp",
+        platform: "windows",
+      };
+    }),
+  ];
+}
---
+++ public/games/Othello/vite.config.ts
@@ -0,0 +1,83 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { Plugin, defineConfig } from "vite";
+
+import { createHtmlPlugin } from "vite-plugin-html";
+import { VitePWA } from "vite-plugin-pwa";
+
+import * as mdi from "@mdi/js";
+import { manifest } from "./manifest";
+
+import { themes } from "./src/theme-data";
+
+export default defineConfig(({ command }) => ({
+  plugins: [
+    createHtmlPlugin({
+      inject: {
+        data: {
+          ...mdi,
+          COMMAND: command,
+          DESCRIPTION: manifest.description,
+          TITLE: manifest.name,
+          BASE_URL: "https://othello-rust.web.app",
+        },
+      },
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeOptionalTags: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        minifyCSS: true,
+        minifyJS: true,
+      },
+    }),
+    VitePWA({
+      manifest,
+      includeManifestIcons: false,
+      includeAssets: ["favicon.ico", "icons/any.svg"],
+      workbox: {
+        sourcemap: true,
+        globPatterns: ["**/*.{html,js,css,wasm,woff2}"],
+      },
+    }),
+    modulepreloadPlugin({ regex: /worker-[0-9a-f]+\.js$/ }),
+  ],
+  build: {
+    target: "esnext",
+    modulePreload: { polyfill: false },
+    sourcemap: true,
+  },
+  define: {
+    __THEME_DATA__: JSON.stringify(themes, undefined, 2),
+  },
+  resolve: {
+    alias: {
+      "rust-othello": fileURLToPath(new URL("./rust-othello/pkg/rust_othello", import.meta.url)),
+    },
+  },
+}));
+
+function modulepreloadPlugin({ regex }: { regex: RegExp }): Plugin {
+  let baseUrl: string;
+
+  return {
+    name: "modulepreload",
+
+    configResolved({ base }) {
+      baseUrl = base;
+    },
+
+    transformIndexHtml(html, { bundle }) {
+      return Object.values(bundle ?? {})
+        .filter((chunk) => regex.test(chunk.fileName))
+        .map((chunk) => ({
+          tag: "link",
+          attrs: { rel: "modulepreload", href: baseUrl + chunk.fileName },
+          injectTo: "head",
+        }));
+    },
+  };
+}
